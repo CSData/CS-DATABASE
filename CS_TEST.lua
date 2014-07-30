4aa44a32aec091045b96fcea8e24a5ba6e303d75
CSData <randomcrawfishofficial@gmail.com>
2014-07-30 19:08:54 -0400

    CS_TEST.lua
    Swag

---
+++ CS_TEST.lua
@@ -0,0 +1,14 @@
+-- ~•~•~•~•~•~•~
+-- ~• crawsec ~•
+-- ~•~•~•~•~•~•~
+-- this is just a test :D
+
+CSMod = require "CSMod"
+
+CSMod.Greet()
+
+CSMod.database()
+
+CSMod.signoff()
+
+print('              CS/LOG_OFF.LUA            ')
